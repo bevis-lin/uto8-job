b708e7d505788dc039747e1c4280b0fec2720fe4
Bevis Lin <bevis.lin@icloud.com>
2022-05-24 18:51:43 +0800

    got vrf number and assign to blindbox

--- GetUbBlindBox.py
+++ GetUbBlindBox.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 import json
@@ -16,7 +17,11 @@ contract_instnace = w3.eth.contract(address=address, abi=abi)
 
 #print(contract_instnace.functions.getBlindBoxInfo(0).call())
 
-print(contract_instnace.functions.blindBoxes.length.call())
+#print(contract_instnace.functions.blindBoxes.length.call())
+
+#blindBox = contract_instnace.functions.blindBoxes(1).call()
+#print(blindBox)
+
 
 blindBox = contract_instnace.functions.blindBoxes(0).call()
 
@@ -26,3 +31,37 @@ dt_object = datetime.fromtimestamp(blindBox[10])
 print("dt_object = ", dt_object)
 
 print(dt_object<datetime.now())
+
+nonce = w3.eth.get_transaction_count('0x37100698B013ce6097453dEf91986EabA6570Ea2')
+
+# Build a transaction that invokes SalesProvider contract's function called generateRandomNumber
+salesprovider_txn = contract_instnace.functions.getRandomNumberForBlindBox(0).buildTransaction({
+  'chainId': 80001,
+  'gas': 10000000,
+  'maxFeePerGas': w3.toWei('2', 'gwei'),
+  'maxPriorityFeePerGas': w3.toWei('1', 'gwei'),
+  'nonce': nonce,
+})
+
+private_key = 'e61290e879f2937226b46788b22a19b1e0ea6642fff7507808bf768f60e4101e'
+signed_txn = w3.eth.account.sign_transaction(salesprovider_txn, private_key=private_key)
+print(signed_txn.hash)
+w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+print(w3.toHex(w3.keccak(signed_txn.rawTransaction)))
+
+receipt = None
+
+while receipt == None:
+  try:
+    receipt = w3.eth.get_transaction_receipt(w3.toHex(w3.keccak(signed_txn.rawTransaction)))
+  except Exception as e:
+    #<class 'web3.exceptions.TransactionNotFound'>
+    print(type(e))
+    print('Unknow error', e)
+    time.sleep(3)
+
+print(receipt)
+if receipt.status==1:
+  print('Transaction completed.')
+
+
