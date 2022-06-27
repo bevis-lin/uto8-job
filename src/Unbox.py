954e425cf8ea7de2e200c70757ad0674a4f662b6
Bevis Lin <bevis.lin@icloud.com>
2022-06-27 11:24:49 +0800

    check is blindbox already unboxed

--- src/Unbox.py
+++ src/Unbox.py
@@ -11,11 +11,14 @@ w3 = Web3(Web3.HTTPProvider(os.environ['Web3_HTTP_Provider']))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 salesProviderAddress = os.environ['SalesProvider_Contract_Address']
 piamonAddress = os.environ['Piamon_Contract_Address']
-f = open('./abi/SalesProvider.json')
+
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+f = open(FILE_DIR + '/abi/SalesProvider.json')
 salesProviderABI = json.load(f)
 f.close()
 
-f = open('./abi/Piamon.json')
+f = open(FILE_DIR + '/abi/Piamon.json')
 piamonProviderABI = json.load(f)
 f.close()
 
@@ -24,59 +27,58 @@ piamonContract_instance = w3.eth.contract(address=piamonAddress, abi=piamonProvi
 
 blindBoxId = int(os.environ['BlindBox_Id'])
 
-#get BlindBox definition
-blindBox = salesProviderContract_instnace.functions.blindBoxes(blindBoxId).call()
+while(True):
 
-#unboxing time
-print(blindBox[10])
+  print("BlindBoxId:"+str(blindBoxId)+", checking blindbox information...")
 
-dt_object = datetime.fromtimestamp(blindBox[10])
-print("dt_object = ", dt_object)
+  #get BlindBox definition
+  blindBox = salesProviderContract_instnace.functions.blindBoxes(blindBoxId).call()
 
-print(dt_object<datetime.now())
+  #check if already unboxed
+  if blindBox[11] > 0:
+    print("BlindBoxId:"+str(blindBoxId)+", already unboxed")
+    sys.exit()
 
-isTimeToUnbox = dt_object<datetime.now()
+  #unboxing time
+  print(blindBox[10])
 
-if not isTimeToUnbox:
-  print("Unbox time is yet to come")
-  sys.exit()
+  dt_object = datetime.fromtimestamp(blindBox[10])
+  print("dt_object = ", dt_object)
 
-contract_owner_address = os.environ['Contract_Owner_Address']
-nonce = w3.eth.get_transaction_count(contract_owner_address)
+  print(dt_object<datetime.now())
 
-# Build a transaction that invokes SalesProvider contract's function called generateRandomNumber
-salesprovider_txn = salesProviderContract_instnace.functions.getRandomNumberForBlindBox(blindBoxId).buildTransaction({
-  'chainId': 80001,
-  'gas': 10000000,
-  'maxFeePerGas': w3.toWei('2', 'gwei'),
-  'maxPriorityFeePerGas': w3.toWei('1', 'gwei'),
-  'nonce': nonce,
-})
+  isTimeToUnbox = dt_object<datetime.now()
 
-private_key = os.environ['Contract_Owner_Key']
-signed_txn = w3.eth.account.sign_transaction(salesprovider_txn, private_key=private_key)
-print(signed_txn.hash)
-tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-print(w3.toHex(w3.keccak(signed_txn.rawTransaction)))
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-print(tx_receipt, file=sys.stdout)
-print(type(tx_receipt))
+  if not isTimeToUnbox:
+    print("BlindBoxId:"+str(blindBoxId)+ ", unbox time is yet to come. Waiting for 60 seconds...")
+    time.sleep(60)
+    continue
 
-# receipt = None
+  contract_owner_address = os.environ['Contract_Owner_Address']
+  nonce = w3.eth.get_transaction_count(contract_owner_address)
 
-# while receipt == None:
-#   try:
-#     receipt = w3.eth.get_transaction_receipt(w3.toHex(w3.keccak(signed_txn.rawTransaction)))
-#   except Exception as e:
-#     #<class 'web3.exceptions.TransactionNotFound'>
-#     print(type(e))
-#     print('Unknow error', e)
-#     time.sleep(3)
+  print("BlindBoxId:"+str(blindBoxId) + ", begin to unbox...")
 
-# print(receipt)
-if tx_receipt.status==1:
-  print('Transaction complete.')
-  #send queue
+  # Build a transaction that invokes SalesProvider contract's function called generateRandomNumber
+  salesprovider_txn = salesProviderContract_instnace.functions.getRandomNumberForBlindBox(blindBoxId).buildTransaction({
+    'chainId': 80001,
+    'gas': 10000000,
+    'maxFeePerGas': w3.toWei('2', 'gwei'),
+    'maxPriorityFeePerGas': w3.toWei('1', 'gwei'),
+    'nonce': nonce,
+  })
 
+  private_key = os.environ['Contract_Owner_Key']
+  signed_txn = w3.eth.account.sign_transaction(salesprovider_txn, private_key=private_key)
+  print(signed_txn.hash)
+  tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+  print(w3.toHex(w3.keccak(signed_txn.rawTransaction)))
+  tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+  print(tx_receipt, file=sys.stdout)
+  print(type(tx_receipt))
 
+  if tx_receipt.status==1:
+    print("BlindBoxId:"+str(blindBoxId)+", transaction complete.")
+  else:
+    print("BlindBoxId:"+str(blindBoxId)+", transaction failed.")
 
