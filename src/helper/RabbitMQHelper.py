b64cb70633c59b54aeed78e34c8d1e8497fb867a
Bevis Lin <bevis.lin@icloud.com>
2022-06-27 15:53:28 +0800

    close rabbitmq connection

--- src/helper/RabbitMQHelper.py
+++ src/helper/RabbitMQHelper.py
@@ -3,13 +3,17 @@ import pika
 import os
 
 credentials = pika.PlainCredentials('uto8','uto8')
-connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RABBITMQ_HOST'],5672,'/',credentials))
-channel = connection.channel()
+
 
 def QueueDeclare(name):
+  connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RABBITMQ_HOST'],5672,'/',credentials))
+  channel = connection.channel()
   channel.queue_declare(queue=name)
+  connection.close()
 
 def Publish(queueName, message):
+  connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RABBITMQ_HOST'],5672,'/',credentials))
+  channel = connection.channel()
   channel.basic_publish(exchange='', routing_key=queueName, body=message)
   print(" [x] Sent "+message)
-
+  connection.close()
